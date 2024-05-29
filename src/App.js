588394aec425a802ba33a56681dd5699cf5b693d
Om <om.dhengle@gmail.com>
2024-05-30 00:21:55 +0530

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -65,37 +65,37 @@ function App() {
       </nav>
 
       <div className="user container my-3">
-        <p>UserName : Om Dhengle </p>
+        <p><b>UserName </b>: Om Dhengle </p>
         <hr/>
       </div>
       <div className="highlights container">
-        <div className="box1 ">Production Today<p className="inner">0 kWh</p></div>
-        <div className="box2 ">Total Production<p className="inner">0 kWh</p></div>
-        <div className="box3 ">Income Today <p className="inner">0 INR</p></div>
-        <div className="box4 ">Total Income <p className="inner">0 INR</p></div>
+        <div className="box1 "><b>Production Today</b><p className="inner">0 kWh</p></div>
+        <div className="box1 "><b>Total Production</b><p className="inner">0 kWh</p></div>
+        <div className="box1 "><b>Income Today </b><p className="inner">0 INR</p></div>
+        <div className="box1 "><b>Total Income </b><p className="inner">0 INR</p></div>
       </div><p></p>
       <div className="overview container">
         <h3>Overview</h3>
         <p className="info mx-3" id="TH">
-          <p>Total Hours : 0</p>
+          <p><b>Total Hours : 0</b></p>
         </p>
         <p className="info mx-3" id="CAP">
-          Capacity : 18 v
+          <b>Capacity</b> : 18 v
         </p>
         <p className="info mx-3" id="RCO">
-          Reduce CO<sub>2</sub> : {data.CO2} t
+          <b>Reduce CO<sub>2</sub></b> : {data.CO2} t
         </p>
         <p className="info mx-3" id="WTH">
-          Weather : {data.Weather}
+          <b>Weather </b>: {data.Weather}
         </p>
         <p className="info mx-3" id="HUM">
-          Humidity : {data.Humidity} %
+          <b>Humidity</b> : {data.Humidity} %
         </p>
         <p className="info mx-3" id="RAM">
-          Rainmeter : {data.Rainmeter} mm
+          <b>Rainmeter</b> : {data.Rainmeter} mm
         </p>
         <p className="info mx-3" id="TEMP">
-          Temperature : {data.Temp} <sup>o</sup>C
+          <b>Temperature </b>: {data.Temp} <sup>o</sup>C
         </p>
       </div>
 
